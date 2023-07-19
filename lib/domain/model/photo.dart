8c9b24425e92d2374e9a0582715060032e355eff
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-19 10:13:50 +0900

    [이미지 검색 앱] freezed 연결

--- lib/domain/model/photo.dart
+++ lib/domain/model/photo.dart
@@ -1,20 +1,20 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'photo.freezed.dart';
 
 part 'photo.g.dart';
 
-@JsonSerializable(explicitToJson: true)
-class Photo {
-  final num id;
-  final String url;
-  final num views;
-
-  Photo({
-    required this.id,
-    required this.url,
-    required this.views,
-  });
-
-  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
-
-  Map<String, dynamic> toJson() => _$PhotoToJson(this);
+@freezed
+class Photo with _$Photo {
+  const factory Photo({
+    required num id,
+    required num views,
+    required num likes,
+    required String webformatURL,
+    required String tags,
+    required String user,
+    required String userImageURL,
+  }) = _Photo;
+  factory Photo.fromJson(Map<String, Object> json) => _$PhotoFromJson(json);
 }
